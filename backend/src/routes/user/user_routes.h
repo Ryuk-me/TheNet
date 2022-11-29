4a0c4b87ec99f673b80e65eba82f8019f30f2ce3
ryuk-me <neerajkr1210@gmail.com>
2022-11-29 14:52:20 +0530

    ✨ /api/v1 path added to user route

--- backend/src/routes/user/user_routes.h
+++ backend/src/routes/user/user_routes.h
@@ -2,6 +2,7 @@
 #include "../routes.h"
 #include "../../models/db.h"
 #include "../../utilities/errors.h"
+#include "../../config/ENV.h"
 
 using crow::status;
 
@@ -10,60 +11,83 @@ class UserRoutes : public RouteCollection
 public:
     static void getRoutes(crow::SimpleApp &app, auto &storage, Database &db)
     {
-        CROW_ROUTE(app, "/user").methods(crow::HTTPMethod::POST, crow::HTTPMethod::PUT, crow::HTTPMethod::GET)([&storage, &db](const crow::request &req, crow::response &res)
-                                                                                                               {
-        if (req.method == crow::HTTPMethod::POST){
-            auto body = crow::json::load(req.body);
-            if(!body){
-                res = crow::response(status::BAD_REQUEST, ERROR::INVALID_BODY);
-                res.end();
-                return;
-            }
-            if(body.has("name") && body.has("course_name") && body.has("firebase_id") && body.has("email")){
-                Database::User u;
-                u.name = body["name"].s();
-                u.courseName = body["course_name"].s();
-                u.firebaseId = body["firebase_id"].s();
-                u.email = body["email"].s();
-                int id = db.insert_user(u,storage);
-                res = crow::response(crow::status::CREATED,ERROR::DETAILS_ADDED);
-                res.end();
-                return;
-            }
-            else{
-                res = crow::response(status::BAD_REQUEST,ERROR::MALFORMED_REQUEST);
-                res.end();
-                return;
-            }
-        }
-        if(req.method == crow::HTTPMethod::GET){
-            if (req.url_params.get("id") != nullptr){
-                std::string fbid = req.url_params.get("id");
-                std::cout<<fbid;
-                auto u = db.get_user(fbid);
-                if(u.size() == 0){
-                    res = crow::response(status::NOT_FOUND,ERROR::USER_NOT_FOUND);
+
+        app.route_dynamic(std::string("/") + ENV::BASE_API_V1 + std::string("/user"))
+            .methods(crow::HTTPMethod::POST,
+                     crow::HTTPMethod::PUT,
+                     crow::HTTPMethod::GET)([&storage, &db](
+                                                const crow::request &req,
+                                                crow::response &res)
+                                            {
+            if (req.method == crow::HTTPMethod::POST)
+            {
+                auto body = crow::json::load(req.body);
+                if (!body)
+                {
+                    res = crow::response(status::BAD_REQUEST, ERROR::INVALID_BODY);
                     res.end();
                     return;
                 }
-                crow::json::wvalue wv;
-                for(auto &user : u) {
-                    wv["id"] = user.id;
-                    wv["name"]= user.name;
-                    wv["firebase_id"] = user.firebaseId;
-                    wv["course_name"] = user.courseName;
-                    wv["email"] =user.email;
-                    res = crow::response(status::OK,wv);
+                if (body.has("name") && body.has("course_name") && body.has("firebase_id") && body.has("email"))
+                {
+                    Database::User u;
+                    u.name = body["name"].s();
+                    u.courseName = body["course_name"].s();
+                    u.firebaseId = body["firebase_id"].s();
+                    u.email = body["email"].s();
+                    try
+                    {
+                        int id = db.insert_user(u, storage);
+                    }
+                    catch (std::system_error &e)
+                    {
+                        res = CUSTOM_ERROR::custom_error(crow::status::CONFLICT, e.what());
+                        res.end();
+                        return;
+                    }
+                    res = crow::response(crow::status::CREATED, ERROR::DETAILS_ADDED);
                     res.end();
                     return;
                 }
-                
-            }else{
-                res = crow::response(status::BAD_REQUEST, ERROR::NOT_VALID_PARAMS);
-                res.end();
-                return;
+                else
+                {
+                    res = crow::response(status::BAD_REQUEST, ERROR::MALFORMED_REQUEST);
+                    res.end();
+                    return;
+                }
             }
-            
-        } });
+            if (req.method == crow::HTTPMethod::GET)
+            {
+                if (req.url_params.get("id") != nullptr)
+                {
+                    std::string fbid = req.url_params.get("id");
+                    std::cout << fbid;
+                    auto u = db.get_user(fbid);
+                    if (u.size() == 0)
+                    {
+                        res = crow::response(status::NOT_FOUND, ERROR::USER_NOT_FOUND);
+                        res.end();
+                        return;
+                    }
+                    crow::json::wvalue wv;
+                    for (auto &user : u)
+                    {
+                        wv["id"] = user.id;
+                        wv["name"] = user.name;
+                        wv["firebase_id"] = user.firebaseId;
+                        wv["course_name"] = user.courseName;
+                        wv["email"] = user.email;
+                        res = crow::response(status::OK, wv);
+                        res.end();
+                        return;
+                    }
+                }
+                else
+                {
+                    res = crow::response(status::BAD_REQUEST, ERROR::NOT_VALID_PARAMS);
+                    res.end();
+                    return;
+                }
+            } });
     };
 };
--- backend/src/utilities/errors.h
+++ backend/src/utilities/errors.h
@@ -8,3 +8,12 @@ namespace ERROR
     crow::json::wvalue USER_NOT_FOUND({{"message", "User not found."}});
     crow::json::wvalue NOT_VALID_PARAMS({{"message", "Not valid parameter/s."}});
 }
+
+namespace CUSTOM_ERROR
+{
+    auto custom_error(int status, auto string)
+    {
+        crow::json::wvalue CUSTOM_ERROR_LOG({{"message", string}});
+        return crow::response(status, CUSTOM_ERROR_LOG);
+    }
+}
